68cd6d4e87ceb2e15c7ed1e91fc18120184df2e9
paulajara <112428434+paulajara@users.noreply.github.com>
2022-08-31 18:28:07 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,20 @@
+basic.forever(function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # # # .
+        . . . . .
+        `)
+    basic.pause(1000)
+    basic.showIcon(IconNames.Ghost)
+    basic.pause(1000)
+})
